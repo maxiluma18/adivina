ed9f0f8cf924ef543cadde6670f6eb550f6eb4c3
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-10-30 11:36:59 -0300

    Adivina Numeros

---
+++ index.js
@@ -0,0 +1,81 @@
+const fetchNums = async () => {
+    try {
+        const response = await fetch('/rta.json');
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+        return null
+    }
+}
+const main = document.querySelector("#numeros")
+const array = []
+const numeros = fetchNums();
+numeros.then(data => {
+    data.forEach(element => {
+        const div = document.createElement("div")
+        div.classList.add("card")
+        div.setAttribute("key", element.id)
+        const divImg = document.createElement("div")
+        divImg.classList.add("card-img")
+        divImg.style.backgroundColor = element.color;
+        divImg.setAttribute("for", element.id)
+        const input = document.createElement("input")
+        input.id = element.id
+        input.type = "number"
+        input.maxLength = 10
+        input.minLength = 1
+        div.appendChild(divImg)
+        div.appendChild(input)
+        main.appendChild(div)
+        array.push(element.rta)
+    });
+
+})
+
+const verificacion = () => {
+    const inputs = document.querySelectorAll("input");
+    let allInputsValid = true; // Variable para verificar si todos los inputs son válidos
+    const booleans = [];
+    let i = 0;
+
+    inputs.forEach(input => {
+
+
+        const inputValue = parseFloat(input.value);
+
+        if (inputValue > 10) {
+            alert("Pista: Hay que ingresar números menores a 2*5");
+            allInputsValid = false;
+            return allInputsValid;
+        }
+
+        if (inputValue === array[i]) {
+            booleans.push(array[i]);
+        }
+        i++;
+    });
+
+    if (booleans.length !== 4) {
+        allInputsValid = false;
+        alert("Números incorrectos, prueba de nuevo")
+    }
+
+    console.log(booleans);
+    console.log(allInputsValid);
+
+    return allInputsValid;
+}
+
+
+const button = document.querySelector("button")
+button.addEventListener("click", e => {
+    e.preventDefault()
+
+    if (verificacion()) {
+        window.location.href = "./victoria.html"
+    }
+
+}
+)
+
